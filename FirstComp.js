150487062b85f915d0dcee5548d8f4ae9e27717a
SilentWatch225 <90773123+SilentWatch225@users.noreply.github.com>
2023-01-25 10:52:58 -0600

    Continued work on button

---
+++ FirstComp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TouchableHighlight, Text, StyleSheet } from 'react-native';
+
+function FirstComp ({title, onPress, buttonColor, textColor, borderColor}){
+    return(
+        <TouchableHighlight
+        onPress = {onPress},
+        buttonColor
+
+
+        </TouchableHighlight>
+    );
+}
