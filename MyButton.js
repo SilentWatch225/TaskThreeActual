a10ff64d0e516f99cab4c1cff4f8dcf957e30042
SilentWatch225 <90773123+SilentWatch225@users.noreply.github.com>
2023-01-25 11:00:22 -0600

    Rename firstcomp.js to mybutton.js

---
+++ MyButton.js
@@ -0,0 +1,16 @@
+import { BackgroundColor } from 'jest-util/node_modules/chalk';
+import React from 'react';
+import { TouchableHighlight, Text, StyleSheet } from 'react-native';
+
+function FirstComp ({title, onPress, underColor, textColor, borderColor}){
+    return(
+        <TouchableHighlight
+        onPress = {onPress}
+        underColor = {buttonColor}
+        color = {textColor}
+        underlayColor = 
+
+
+        </TouchableHighlight>
+    );
+}
